3c82a78acfc7b30bf237201c89f496a28ea15fe2
Mahsa Arabameri <amerimahsa@yahoo.com>
2020-12-02 20:18:34 +0000

    WASD Movement

--- input.js
+++ input.js
@@ -10,7 +10,18 @@ const setupInput = function() {
 };
 
 const handleUserInput = function(key) { //function to contrlo cntrl+c
-  
+  if (key === "w") {
+    console.log("up");
+  }
+  if (key === "a") {
+    console.log("left");
+  }
+  if (key === "s") {
+    console.log("down");
+  }
+  if (key === "d") {
+    console.log("right");
+  }
   if (key === '\u0003') {
     process.exit();
   }
